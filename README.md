7f66a1e19e26fb24d77485f13e60597e69887a4d
leberKleber <max.marche@live.de>
2019-01-14 23:53:42 +0100

    make wait script print wrong values

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wait-until-curl
 Dockerfile which wait until curl response become to expected value
+
+## Example
+```bash
+ docker run --rm -e "WUC_EXPECTED=200" -e "WUC_WRITE_OUT=%{http_code}" -e "WUC_URL=http://myservice:8080/internal/health" --network "myservice_default" leberkleber/wait_until_curl
+```
--- wait-until-curl.sh
+++ wait-until-curl.sh
@@ -4,9 +4,11 @@ URL="${WUC_URL?Need to set WUC_URL}"
 WRITE_OUT="${WUC_WRITE_OUT?Need to set WUC_WRITE_OUT}"
 EXPECTED="${WUC_EXPECTED?Need to set WUC_EXPECTED}"
 
-CURL="curl -s -o /dev/null -w ''$WRITE_OUT'' $URL"
-while [ "$($CURL)" != "$EXPECTED" ]
+CURL="curl -s -o /dev/null -w $WRITE_OUT $URL"
+CURL_RESPONSE=$($CURL)
+while [ $CURL_RESPONSE != "$EXPECTED" ]
 do
-echo "expected value not reached"
-sleep 1
+    echo "expected value not reached given: '$CURL_RESPONSE', expected: '$EXPECTED'"
+    sleep 1
+    CURL_RESPONSE=$($CURL)
 done
